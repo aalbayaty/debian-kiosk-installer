a8b1c302419c391a6c7ada0d981a75c1e14e8cac
aalbayaty <697721+aalbayaty@users.noreply.github.com>
2025-06-16 13:17:57 +0300

    Update kiosk-installer-gy.sh

--- preseed/kiosk-installer-gy.sh
+++ preseed/kiosk-installer-gy.sh
@@ -2,10 +2,14 @@
 
 # dir
 mkdir -p /target/home/kiosk/.config/openbox
+mkdir -p /target/etc/X11
+mkdir -p /target/etc/lightdm
+
+
 
 # remove virtual consoles
 if [ -e "/target/etc/X11/xorg.conf" ]; then
-  mv /etc/X11/xorg.conf /etc/X11/xorg.conf.backup
+  mv /target/etc/X11/xorg.conf /target/etc/X11/xorg.conf.backup
 fi
 cat > /target/etc/X11/xorg.conf << EOF
 Section "ServerFlags"
@@ -15,7 +19,6 @@ EOF
 
 # create config
 if [ -e "/target/etc/lightdm/lightdm.conf" ]; then
-  mkdir -p /target/etc/lightdm
   mv /target/etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.backup
 fi
 cat > /target/etc/lightdm/lightdm.conf << EOF
@@ -26,7 +29,7 @@ EOF
 
 # create autostart
 if [ -e "/target/home/kiosk/.config/openbox/autostart" ]; then
-  mv /target/home/kiosk/.config/openbox/autostart /home/kiosk/.config/openbox/autostart.backup
+  mv /target/home/kiosk/.config/openbox/autostart /target/home/kiosk/.config/openbox/autostart.backup
 fi
 cat > /target/home/kiosk/.config/openbox/autostart << EOF
 #!/bin/bash
@@ -54,5 +57,7 @@ xset s noblank
   sleep 5
 done &
 EOF
+
+chmod 777 /target/home/kiosk/.config/openbox -R
 
 echo "Done!"
