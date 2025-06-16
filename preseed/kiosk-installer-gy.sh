33dd2c6626902120c06cac695092176fd4c873db
aalbayaty <697721+aalbayaty@users.noreply.github.com>
2025-06-16 12:48:23 +0300

    Create kiosk-installer-gy.sh

---
+++ preseed/kiosk-installer-gy.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# dir
+mkdir -p /target/home/kiosk/.config/openbox
+
+# remove virtual consoles
+if [ -e "/target/etc/X11/xorg.conf" ]; then
+  mv /etc/X11/xorg.conf /etc/X11/xorg.conf.backup
+fi
+cat > /target/etc/X11/xorg.conf << EOF
+Section "ServerFlags"
+    Option "DontVTSwitch" "true"
+EndSection
+EOF
+
+# create config
+if [ -e "/target/etc/lightdm/lightdm.conf" ]; then
+  mv /target/etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.backup
+fi
+cat > /target/etc/lightdm/lightdm.conf << EOF
+[SeatDefaults]
+autologin-user=kiosk
+user-session=openbox
+EOF
+
+# create autostart
+if [ -e "/target/home/kiosk/.config/openbox/autostart" ]; then
+  mv /target/home/kiosk/.config/openbox/autostart /home/kiosk/.config/openbox/autostart.backup
+fi
+cat > /target/home/kiosk/.config/openbox/autostart << EOF
+#!/bin/bash
+
+unclutter -idle 0.1 -grab -root &
+
+while :
+do
+# xrandr -o left the screen will be to the left
+xrandr -o left
+xset -dpms
+xset s off
+xset s noblank
+  chromium \
+    --no-first-run \
+    --start-maximized \
+    --disable \
+    --disable-translate \
+    --disable-infobars \
+    --disable-suggestions-service \
+    --disable-save-password-bubble \
+    --disable-session-crashed-bubble \
+    --incognito \
+    --kiosk "https://muslimhub.net/public/location/Guyana331/?Settings=tv"
+  sleep 5
+done &
+EOF
+
+echo "Done!"
